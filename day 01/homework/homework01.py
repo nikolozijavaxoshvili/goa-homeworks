96b2f42aea97a713f70e34f53e4b140574e4fe7f
nikolozijavaxoshvili <nikolozijavaxishvili1@gmail.com>
2024-09-12 17:27:20 +0400

    first commit

---
+++ day 01/homework/homework01.py
@@ -0,0 +1,18 @@
+name = "nikoloz"
+
+
+ #print (name)
+
+
+
+
+age = ("13 years")
+
+Height = ("170 sm")
+
+surname = ("javaxishvili")
+
+
+print (name +" "+ surname +" "+ str (age) +" "+ str (Height)  ) 
+print ("klasi 8 b")
+print (" N5 sajaro skola ")
