cf68dea1339e4ef23152a2e21ccc0ee3796888bd
nikolozijavaxoshvili <nikolozijavaxishvili1@gmail.com>
2024-10-25 22:01:29 +0400

    commit

---
+++ day 07/classwork/classwork.py
@@ -0,0 +1,9 @@
+goa = 212121
+
+goa2 = "321"
+
+goa3 = 5.6
+
+user_age = input()
+
+print (user_age)
---
+++ day 07/homework/homework.py
@@ -0,0 +1,39 @@
+#task1
+age = 15
+
+height = 160
+
+weith = 50
+
+aura = -0.1
+
+print(type(age))
+
+print(type(height))
+
+print(type(weith))
+
+print(type(aura))
+#task2
+
+loved= input ("დაწერე შენი საყვარელი პროგრამირების ენა")
+print (loved)
+#task 3
+
+
+age = int(input("enter your age: "))
+future = int(input("enter how many years you want to be in future: "))
+print(age + future)
+
+
+
+
+#task 4
+mom = input("enter your mothers name and age ")
+
+dad = input("enter your dads name and age")
+
+brother = input(" if you dont have brother and age type : idont have brother ")
+
+sister =  input (" if you dont have sister and age type : idont have sister ")
+
