7529fd43d3734a3a24782308ebf477c988099918
nikolozi1234532 <nana.qarcxia8282@gmail.com>
2024-11-20 21:53:07 +0400

    first commit

---
+++ day 09/homework/homework.py
@@ -0,0 +1,31 @@
+num1 = 76
+
+num2 = 99
+
+print (num1 > num2 )
+
+print (num1 < num2)
+
+print (num2 >= num1)
+
+print (num1 <= num2)
+
+print (num2 != num1 )
+
+print (num1 == num2)
+
+
+
+num33 =int( input ("enter your first number here") )
+
+num11 =int( input ("enter your first number here") )
+
+print (num33 > num11) 
+
+
+# < > ეს ოპერატორები გამოგვადგება იმისთვის რომ ერთი რიცხვი შევადაროთ მეორე რიცხვს
+
+# < ნაკლებობა გამოგვაგდება იმისთვის რომ გავიგოთ რომელი რიცხვი არის რომელზე ნაკლები
+
+# > დამატება გამოგვადგება იმისთვის რომ გავიგოთ რომელი რიცხვი არის რომელზე დიდი
+
---
+++ day 11/claaswork/classwork.py
@@ -0,0 +1,6 @@
+a = (5 + 3 == 8)
+b = (10 - 5 == 3)
+
+print(a and not b) 
+print(a and b)
+
