6d1946c7abd1c7ab581022956b7ec9f1ea2dd082
nikolozi1234532 <nana.qarcxia8282@gmail.com>
2024-12-06 16:10:02 +0400

    commit

---
+++ day 09/classwork/classwork.py
@@ -0,0 +1,13 @@
+inted  = 21
+
+strung = "goa"
+
+fluat = 9.3
+
+print (inted, strung, fluat)
+
+print (type( inted ))
+
+print (type( strung ))
+
+print (type( fluat ))
---
+++ day 13/classwork/classwork.py
@@ -0,0 +1,45 @@
+
+
+
+print (100 < 10)
+
+print (567 > 10)
+
+print (10 >= 10)
+
+print (3434 > 89)
+
+print (12 >= 12)
+
+print (80 < 76)
+
+print (213 > 12)
+
+print (1234 < 123421)
+
+print (59 > 21)
+
+print (10 >= 10)
+
+ 
+
+ 
+print (1234 >=21)
+
+print (123 != 21)
+
+print (123 and 21)
+
+print (131 or 131)
+
+print (990 and 97)
+
+print (809 or 321)
+
+print (7878 and 21)
+
+print (887 or 321)
+
+print (66 and 21)
+
+print (213 or 44)
