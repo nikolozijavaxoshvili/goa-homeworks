b8e4c0bf16bafc358b144a2a7f3caaf8ef4e0281
nikolozi1234532 <nana.qarcxia8282@gmail.com>
2024-12-19 21:16:31 +0400

    commit

---
+++ day 15/homework/homework.py
@@ -0,0 +1,20 @@
+# task 1 
+
+# ალგორითმი არის ნაბიჯ ნაბიჯ დახამარება 
+
+
+
+
+
+# task 2
+for i in range (100):
+    print(i)
+
+# task 3
+for i in range (152):
+    print (i)
+
+
+for i in range (101):
+    print ("goa is best",i )
+
